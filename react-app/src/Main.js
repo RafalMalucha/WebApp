8d320c186446307454aebe9a8f89632851e7306f
RafalMalucha <s220986@student.uek.krakow.pl>
2022-06-26 02:37:32 +0200

    fix

---
+++ react-app/src/Main.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+function Main() {
+    return (
+        <div class='justify-center align-middle'>
+            <div class='container mx-auto px-5 bg-slate-600'>
+                <p>
+                    aaaaaaaaaaaaaaaaaaaaaaaaaa
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import Navbar from './Navbar.js';
+import Main from './Main.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Navbar/>
+    <Main/>
   </React.StrictMode>
 );
 
