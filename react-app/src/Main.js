968db4341d58c7338b4cb65a2267f561df9e6c60
RafalMalucha <s220986@student.uek.krakow.pl>
2022-06-26 11:55:35 +0200

    main page two columns

--- react-app/src/Main.js
+++ react-app/src/Main.js
@@ -3,10 +3,39 @@ import './index.css'
 function Main() {
     return (
         <div class='justify-center align-middle'>
-            <div class='container mx-auto px-5 bg-slate-600'>
-                <p>
-                    aaaaaaaaaaaaaaaaaaaaaaaaaa
-                </p>
+            <div class='flex flex-row px-2 space-x-2 bg-primary'>
+                <div class='basis-1/5'>
+                    <div class='container rounded mx-auto px-5 py-2 mt-2 bg-neutral'>
+                        <ul>
+                            <li>
+                                <div class=''>
+                                    <p>
+                                        aaaaaaaaaaaaaaaaaaaaaaaaaa
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <p>
+                                    aaaaaaaaaaaaaaaaaaaaaaaaaa
+                                </p>                                
+                            </li>
+                            <li>
+                                <p>
+                                    aaaaaaaaaaaaaaaaaaaaaaaaaa
+                                </p>
+                            </li>                                                        
+                        </ul>
+                    </div>
+                </div>
+                <div class='basis-4/5'>
+                    <div class='container rounded px-5 py-2 mt-2 bg-neutral'>
+                        <div class='items-center content-center justify-center text-center'>
+                            <p>
+                                aaaaaaaaaaaaaaaaaaaaaaaaaa
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
