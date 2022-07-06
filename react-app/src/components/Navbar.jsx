ffb42c98662b9b4b8a99765362992df9e63110f1
RafalMalucha <s220986@student.uek.krakow.pl>
2022-07-06 09:40:21 +0200

    buttons

--- react-app/src/components/Navbar.jsx
+++ react-app/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div class="navbar sticky bg-primary shadow-xl">
-        <div class='flex'>
+    <div class="navbar sticky bg-primary shadow-xl flex flex-row">
+        <div class='basis-1/4'>
             <div class='flex-none'>
                 <button class="btn btn-square btn-ghost">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -12,9 +12,14 @@ const Navbar = () => {
             <div class='flex-1'>
                 <a class="btn btn-ghost normal-case text-xl text-accent font-bold italic hover:text-cyan-400" href='#'>ReAPP</a>
             </div>
-            <div class='flex-none'>
-                <button class="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        </div>
+        <div class='basis-3/4'>
+            <div class='justify-itmes-end'>
+                <button class='rounded-full text-white bg-slate-500'>
+                    sign in
+                </button>
+                <button class='rounded-full text-white bg-slate-500'>
+                    sign up
                 </button>
             </div>
         </div>
