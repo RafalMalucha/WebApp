26262bb8b167e22fa3252000b57a83c9575602cf
RafalMalucha <s220986@student.uek.krakow.pl>
2022-07-07 16:04:06 +0200

    smh

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,9 @@ import React, {useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AddPost from './components/AddPost';
+import Home from './pages/Home';
 import './index.css';
+import { AuthContextProvider } from './components/context/AuthContext';
 
 const App = () => {
   return (
