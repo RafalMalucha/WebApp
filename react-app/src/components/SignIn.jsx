e2bd212eab3d6484184d3f3b8f8edffa490d4848
RafalMalucha <s220986@student.uek.krakow.pl>
2022-07-12 20:12:09 +0200

    add login functionality

--- react-app/src/components/SignIn.jsx
+++ react-app/src/components/SignIn.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { UserAuth } from "../contexts/AuthContext";
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const { signIn } = UserAuth();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('')
+        try {
+            await signIn(email, password)
+        } catch (e) {
+            setError(e.message)
+            console.log(e.message)
+        }
+    };
+
     return (
         <div class='max-w-2xl mx-auto rounded-xl bg-primary/75'>
             <p class='text-white mx-5 pt-5 font-semibold text-xl'></p>
-            <form class='grid grid-cols-5 gap-2'>
+            <form class='grid grid-cols-5 gap-2' onSubmit={handleSubmit}>
                 <div class='col-start-2 col-end-5 bg-slate-600 rounded-md mx-5 mt-1.5 mb-2'>                   
-                    <input id='email'
+                    <input 
+                    onChange={(e) => setEmail(e.target.value)}
                     type="email" 
                     placeholder='e-mail..' 
                     class='input input-bordered input-sm bg-slate-400 placeholder-gray-700 w-full'
                     required />                    
                 </div>
                 <div class='col-start-2 col-end-5 bg-slate-600 rounded-md mx-5 mt-1.5 mb-2'>
-                    <input id='password'
+                    <input 
+                    onChange={(e) => setPassword(e.target.value)}
                     type="password" 
                     placeholder='password..' 
                     class='input input-bordered input-sm bg-slate-400 placeholder-gray-700 w-full max-w-full'
@@ -21,12 +40,11 @@ const Login = () => {
                 </div>
                 <div class='col-start-1 col-end-6 bg-accent rounded-md mx-5 my-1.5'>
                     <p class='text-center'>
-                        <button type='btn btn-ghost'>
+                        <button type='btn btn-ghost' onClick={console.log('aaa')}>
                             <p class='normal-case text-3xl text-black text-center font-bold hover:text-cyan-400 mt-1 mb-2'>SIGN IN</p>
                         </button>
                     </p>
                 </div>
-                <div class='containter bg-primary h-3 ml-5'></div>
             </form>
             <div>
             </div>
